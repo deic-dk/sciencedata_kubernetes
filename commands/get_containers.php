c108095b08384f45c6929b4b6b42d16adfcfbb94
deltafunction <frederik@orellana.dk>
2021-02-02 00:12:07 +0100

    require user

--- commands/get_containers.php
+++ commands/get_containers.php
@@ -1,5 +1,20 @@
 <?php
 
-echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers`;
+// curl '10.0.0.12/get_containers.php?user_id=fror@dtu.dk'
+
+$owner = $_GET['user_id']; // ID of the user logged into ScienceData
+
+if(empty($owner)){
+	header($_SERVER['SERVER_PROTOCOL'] . " 403 Forbidden", true, 403);
+	echo "<h1>Missing parameter(s)!</h1>";
+	exit;
+}
+
+if(strpos($_SERVER['REMOTE_ADDR'], '10.0')!==0){
+	header($_SERVER['SERVER_PROTOCOL'] . " 403 Forbidden", true, 403);
+	exit;
+}
+
+echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers -u "$owner"`;
 
 ?>
