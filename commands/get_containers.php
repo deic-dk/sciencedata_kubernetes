378a06447fe7083cacbbf7ae818a08cba23d64f0
Frederik Orellana <frederik@orellana.dk>
2021-06-02 00:40:54 +0200

    Bug fixes, API updates, manifests moved to separate repo

--- commands/get_containers.php
+++ commands/get_containers.php
@@ -8,17 +8,19 @@ if(strpos($_SERVER['REMOTE_ADDR'], '10.0')!==0){
 }
 
 $owner = $_GET['user_id']??""; // ID of the user logged into ScienceData
-$fields = $_GET['fields']; // Get just the fields
+$fields = $_GET['fields']; // Get just the fields, the data or both fields and data
 
-if(empty($fields)){
+if(empty($fields) || $fields!="include" || $fields!="yes" || $fields!="true"){
 	/*header($_SERVER['SERVER_PROTOCOL'] . " 403 Forbidden", true, 403);
 	echo "<h1>Missing parameter(s)!</h1>";
 	exit;*/
+	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "$owner" 2>&1 | grep -v '^#'`;
+}
+elseif($fields=="include"){
 	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "$owner"`;
 }
-else{
-	// TODO: not very efficient. Consider adding flags to script.
-	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "$owner" 2>&1 | head -2 | tail -1`;
+else{// only fields - fields=yes or fields=true
+	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "$owner" 2>&1 | head -2 | tail -1 | sed -E 's|^#||'`;
 }
 
 ?>
--- commands/run_pod.php
+++ commands/run_pod.php
@@ -10,10 +10,10 @@ if(strpos($_SERVER['REMOTE_ADDR'], '10.0')!==0){
 $owner = $_GET['user_id']; // ID of the user logged into ScienceData and starting the pod
 $storage_path = empty($_GET['storage_path'])?"":$_GET['storage_path']; // Path to mount relative to the URI /storage/
 $public_key = empty($_GET['public_key'])?"":$_GET['public_key']; // SSH public key of the user
-$yaml_uri = $_GET['yaml_uri']; // URI of the YAML file to apply - read using the supplied user ID if it starts with
+$yaml_url = $_GET['yaml_url']; // URL of the YAML file to apply - read using the supplied user ID if it starts with
 															// /shared/, /files/, /group/, /sharingin/, /storage/, otherwise read with admin privileges (system/app file).
 
-if(empty($owner) || empty($yaml_uri)){
+if(empty($owner) || empty($yaml_url)){
 	header($_SERVER['SERVER_PROTOCOL'] . " 500 Internal Server Error", true, 500);
 	echo "<h1>Missing parameter(s)!</h1>";
 	exit;
@@ -23,9 +23,13 @@ $output = null;
 $retval = null;
 $server_ip = $_SERVER['REMOTE_ADDR'];
 
-$yaml_uri = ltrim($yaml_uri, "/");
-if(strpos($yaml_uri, "shared")===0 || strpos($yaml_uri, "files")===0 || strpos($yaml_uri, "group")===0 || strpos($yaml_uri, "sharingin")===0 || strpos($yaml_uri, "sharingin")===0){
+// If it's a path/URI, assume it's on the originating silo and prepend its URL
+$yaml_url = ltrim($yaml_url, "/");
+if(strpos($yaml_url, "shared")===0 || strpos($yaml_url, "files")===0 || strpos($yaml_url, "group")===0 || strpos($yaml_url, "sharingin")===0 || strpos($yaml_url, "storage")===0){
 	$server_ip = urlencode($owner).'@'.$server_ip;
+}
+if(strpos($yaml_url, "https://")===false){
+	$yaml_url = "https://".$server_ip.'/'.$yaml_url;
 }
 
 $arrContextOptions=array(
@@ -35,8 +39,7 @@ $arrContextOptions=array(
 	),
 );
 
-$url = "https://".$server_ip.'/'.$yaml_uri;
-$yaml = file_get_contents($url, false, stream_context_create($arrContextOptions));
+$yaml = file_get_contents($yaml_url, false, stream_context_create($arrContextOptions));
 $tmpfile = tmpfile();
 fwrite($tmpfile, $yaml);
 $metadata = stream_get_meta_data($tmpfile);
@@ -55,7 +58,7 @@ if($retval===0){
 }
 else{
 	header($_SERVER['SERVER_PROTOCOL'] . " 500 Internal Server Error", true, 500);
-	echo "<h1>Something went wrong! Check existence of $url</h1>";
+	echo "<h1>Something went wrong! Check existence of $yaml_url</h1>";
 	echo "<pre>".implode("\n", $output)."</pre>";
 }
 
