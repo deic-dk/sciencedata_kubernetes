95001769be55b2a7a94c2f3de68ef390beb56570
deltafunction <frederik@orellana.dk>
2022-04-01 10:08:19 +0200

    Beginning collaboration with Joshua
    Some uncommitted changes, wanted to have a final commit before
    having multiple collaborators

--- commands/run_pod.php
+++ commands/run_pod.php
@@ -1,23 +1,15 @@
 <?php
 
-// curl '10.0.0.12/run_pod.php?user_id=fror@dtu.dk&storage_path=www&public_key=ssh-rsa%20AAAAB3NzaC1yc2EAAAABIwAAAIEA1lUNAcCuUvl2nxu0ILt0zfdESUmOGlktcDbv8ufRcJ6A1oYDksn%2BFHxxWU3X7laD7dfF9BBkLr5nC3M7ZuuoW1j2QcHcdFRSfTSLuSYM%2FebHdR5g65gGJWrc8qCaFEWS2unLz6rbCqtKBscQDsLtosIXx1brOmWFATWm%2FuCvABc%3D%20frederik%40pcitapi34&yaml_uri=/files/tmp/ubuntu_sciencedata.yaml'
+$logFile = "/var/log/kube.log";
 
-if(strpos($_SERVER['REMOTE_ADDR'], '10.0')!==0){
-	header($_SERVER['SERVER_PROTOCOL'] . " 403 Forbidden", true, 403);
-	exit;
-}
+// curl '10.0.0.12/run_pod.php?user_id=fror@dtu.dk&storage_path=www&public_key=ssh-rsa%20AAAAB3NzaC1yc2EAAAABIwAAAIEA1lUNAcCuUvl2nxu0ILt0zfdESUmOGlktcDbv8ufRcJ6A1oYDksn%2BFHxxWU3X7laD7dfF9BBkLr5nC3M7ZuuoW1j2QcHcdFRSfTSLuSYM%2FebHdR5g65gGJWrc8qCaFEWS2unLz6rbCqtKBscQDsLtosIXx1brOmWFATWm%2FuCvABc%3D%20frederik%40pcitapi34&yaml_uri=/files/tmp/ubuntu_sciencedata.yaml'
 
 $owner = $_GET['user_id']; // ID of the user logged into ScienceData and starting the pod
 $storage_path = empty($_GET['storage_path'])?"":$_GET['storage_path']; // Path to mount relative to the URI /storage/
 $public_key = empty($_GET['public_key'])?"":$_GET['public_key']; // SSH public key of the user
+$file = empty($_GET['file'])?"":$_GET['file']; // File top open inside pod
 $yaml_url = $_GET['yaml_url']; // URL of the YAML file to apply - read using the supplied user ID if it starts with
 															// /shared/, /files/, /group/, /sharingin/, /storage/, otherwise read with admin privileges (system/app file).
-
-if(empty($owner) || empty($yaml_url)){
-	header($_SERVER['SERVER_PROTOCOL'] . " 500 Internal Server Error", true, 500);
-	echo "<h1>Missing parameter(s)!</h1>";
-	exit;
-}
 
 $output = null;
 $retval = null;
@@ -45,10 +37,12 @@ fwrite($tmpfile, $yaml);
 $metadata = stream_get_meta_data($tmpfile);
 $tmpfile_name = $metadata['uri'];
 
-exec('export KUBECONFIG=/etc/kubernetes/admin.conf; run_pod -o "'.$owner.
-		'" -s '.$_SERVER['REMOTE_ADDR'].
-		' -k "'.$public_key.'" -r "'.$storage_path.'" "'.
-		$tmpfile_name.'" 2>&1', $output, $retval);
+$cmd = 'export KUBECONFIG=/etc/kubernetes/admin.conf'.
+		(empty($file)?'':'; export FILE="'.$file.'"').'; run_pod -o "'.$owner.
+		'" -s '.$_SERVER['REMOTE_ADDR'].' -k "'.$public_key.'" -r "'.
+		$storage_path.'" "'.$tmpfile_name.'" 2>&1 | tee -a "'.$logFile.'"';
+
+exec($cmd, $output, $retval);
 
 fclose($tmpfile);
 
@@ -58,7 +52,7 @@ if($retval===0){
 }
 else{
 	header($_SERVER['SERVER_PROTOCOL'] . " 500 Internal Server Error", true, 500);
-	echo "<h1>Something went wrong! Check existence of $yaml_url</h1>";
+	echo "<h1>Something went wrong with $cmd! Check existence of $yaml_url</h1>";
 	echo "<pre>".implode("\n", $output)."</pre>";
 }
 
