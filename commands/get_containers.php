1bc5d023f298af5cc29257616323a53dea9c8bf8
Frederik Orellana <frederik@orellana.dk>
2023-06-18 18:18:31 +0200

    Cleanup

--- commands/get_containers.php
+++ commands/get_containers.php
@@ -7,13 +7,15 @@ if(strpos($_SERVER['REMOTE_ADDR'], '10.0')!==0){
 	exit;
 }
 
-$owner = $_GET['user_id']??""; // ID of the user logged into ScienceData
+$owner = $_GET['user_id']; // ID of the user logged into ScienceData
 $fields = $_GET['fields']; // Get just the fields, the data or both fields and data
 
+if(empty($owner)){
+	header($_SERVER['SERVER_PROTOCOL'] . " 401 Unauthorized", true, 401);
+	exit;
+}
+
 if(empty($fields) || $fields!="include" || $fields!="yes" || $fields!="true"){
-	/*header($_SERVER['SERVER_PROTOCOL'] . " 403 Forbidden", true, 403);
-	echo "<h1>Missing parameter(s)!</h1>";
-	exit;*/
 	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "$owner" 2>&1 | grep '|' | grep -v '^#'`;
 }
 elseif($fields=="include"){
