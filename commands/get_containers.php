25ecab2c9131089a23723728ad721dadb6e55c6c
Frederik Orellana <frederik@orellana.dk>
2026-09-04 23:58:13 +0200

    user_id bug fix.

--- commands/get_containers.php
+++ commands/get_containers.php
@@ -1,6 +1,6 @@
 <?php
 
-// curl '10.0.0.12/get_containers.php?user_id=fror@dtu.dk'
+// curl '10.0.0.12/get_containers.php?user_id=fror@dtu.dk&pod_ip=10.2.x.y'
 
 // We only allow requests from the silos
 if(strpos($_SERVER['REMOTE_ADDR'], '10.0')!==0){
@@ -18,7 +18,7 @@ function checkpassword($passwd){
 
 $_GET = array_map(function($x){return escapeshellcmd($x);}, $_GET);
 
-$owner = $_GET['user_id']; // ID of the user logged into ScienceData
+$owner = empty($_GET['user_id'])&&!empty($pod_ip)?"none":$_GET['user_id']; // ID of the user logged into ScienceData
 $password = empty($_GET['password'])?'':$_GET['password']; // Only used if user_id is empty
 $fields = $_GET['fields']; // Get just the fields, the data or both fields and data
 $pod_ip = $_GET['pod_ip']; // Get just one pod - used by sciencedata to check who owns the pod a given request is coming from
@@ -30,14 +30,18 @@ if(empty($owner)){
 	}
 }
 
+if(!empty($pod_ip)){
+	$pod_ip = " ".$pod_ip;
+}
+
 if(empty($fields) || $fields=="include"){
-	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "${owner}${pod_ip}" 2>&1 | grep '|' | grep -v '^#'`;
+	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "${owner}"${pod_ip} 2>&1 | grep '|' | grep -v '^#'`;
 }
 elseif($fields=="yes" || $fields=="true"){// only fields 
-	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "${owner}${pod_ip}" 2>&1 | head -2 | tail -1 | sed -E 's|^#||'`;
+	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "${owner}"${pod_ip} 2>&1 | head -2 | tail -1 | sed -E 's|^#||'`;
 }
 else{// fields=no , i.e. only values
-	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "${owner}${pod_ip}"`;
+	echo `export KUBECONFIG=/etc/kubernetes/admin.conf; get_containers "${owner}"${pod_ip}`;
 }
 
 ?>
